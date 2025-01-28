15d51762287f686296927914260ada9c39b5a886
sjondev <jonsilvasantos7@gmail.com>
2025-01-28 17:17:34 -0300

    criando o Create do EntityFramework

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Blog_entityframework
         {
             using (var context = new DataContext()) 
             {
+                // Create with TRAKING
                 var tag = new Tag {Name = "Program", Slug = "aspnet-program"};
                 context.Tags.Add(tag);
                 context.SaveChanges(); // Save changes pega os todos os valores que estão na memoria e salva do banco de dados
