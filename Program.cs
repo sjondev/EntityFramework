df00c32988f68d8f8b89ad4400be4813e2cc9988
sjondev <jonsilvasantos7@gmail.com>
2025-01-28 19:05:39 -0300

    trabalhando com filtros no entityFramework

--- Program.cs
+++ Program.cs
@@ -11,16 +11,11 @@ namespace Blog_entityframework
         {
             using (var context = new DataContext()) 
             {
-               var list = context.Posts
-                    .AsNoTracking() // Serve para melhorar a performace de consulta, não vale a pena trabalhar com ele em Delete e Update porque pode dar uma cagadinha caso ele esteja relacionado com alguma tabela que tem chave estrangeira.
-                    .ToList();
-
-                foreach (var post in list)
-                {
-                    Console.WriteLine($"Post: {post.Title}");
-                    Console.WriteLine($"Sumario: {post.Summary}");
-                    Console.WriteLine($"Data de criação: {post.CreateDate}");
-                }
+                var item = context.Posts
+                    .AsNoTracking()
+                    .FirstOrDefault(x => x.Id == 50); // Aqui vou trabalhar as ferramentas de filtragem para mostrar como lidar com um erro caso não exista algum valor
+                Console.WriteLine(item?.Title); // Aqui voce pode analisar que estou colocando um PONTO DE INTERROGAÇÃO na frente do "item?" ele evita dar um erro de throw new Exception
+                Console.WriteLine(item.Slug); // Ao contrario desse aqui que vai estar sem e pode dar um erro
             }
         }
     }
