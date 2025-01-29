089dec4923b8ba53b92f7abbde8623b79680fd26
sjondev <jonsilvasantos7@gmail.com>
2025-01-29 19:16:47 -0300

    Levando algumas novas abordagens para fazer quando queremos ver o log das consultas SQL e evitar o uso de SUBSELECTS

--- Program.cs
+++ Program.cs
@@ -18,10 +18,11 @@ namespace Blog_entityframework
                 // .Where(x => x.AuthorId == 7)
                 .Include(x => x.Author) // o include ele faz um INNER JOIN para trazer os valores da tabela.
                 .Include(x => x.Category)
+                .ThenInclude(x => x.Name) // evitar essa abordagem porque pode trazer SUBSELECTS e isso pode gerar perca de perfomace nas consultas
                 .OrderByDescending(x => x.LastUpdateDate)
                 .ToList();
 
-            foreach (var post in posts) Console.WriteLine($"{post.Title} escrito por {post.Author?.Name} em {post.Category?.Name}");
+            foreach (var post in posts) Console.WriteLine($"{post.Title} escrito por {post.Author?.Name} em categoria {post.Category?.Name}");
         }
     }
 }
