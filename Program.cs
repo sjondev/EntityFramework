44e4a7938d31a1b73bb9ce726729e97e98adf79e
sjondev <jonsilvasantos7@gmail.com>
2025-01-31 18:11:56 -0300

    trabalhando com paginação de dados

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Blog_entityframework.Data;
 using Blog_entityframework.Models;
@@ -11,7 +12,22 @@ namespace Blog_entityframework
         static void Main(string[] args)
         {
             using var context = new DataContext();
-            
+        }
+
+
+        public static List<Post> GetPosts (DataContext context, int skip = 0, int take = 25)
+        {
+            /*
+                Utilizando uma paginação caso um dia se minha base de dados tiver 1milhão de linhas 
+                pelomenos quando for trazer esses dados eu vou trazer no maximo 25 em 25
+            */
+            var posts = context.Posts
+                .AsNoTracking()
+                .Skip(skip)
+                .Take(take)
+                .ToList();
+
+            return posts;
         }
     }
 }
